10dbdc0b6241eaac219e5cedab230649679e8b0a
gab <gabrielbordeaux@gmail.com>
2024-09-22 10:36:25 -0400

    Bump sqlcipher3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'tabulate',
         'passwordgenerator',
         'SQLAlchemy==1.4.41',
-        'sqlcipher3==0.4.5'
+        'sqlcipher3==0.5.3'
     ],  # external dependencies
     entry_points={
         'console_scripts': [
